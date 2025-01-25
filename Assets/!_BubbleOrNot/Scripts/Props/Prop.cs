b0d36cc4f15c69d1e1dbecc26ba434547217a305
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:51:22 +0100

    Update Prop.cs

--- Assets/!_BubbleOrNot/Scripts/Props/Prop.cs
+++ Assets/!_BubbleOrNot/Scripts/Props/Prop.cs
@@ -13,6 +13,27 @@ namespace BubbleOrNot.Runtime
     
     public abstract class Prop : MonoBehaviour
     {
-        public abstract void OnToolUsed(ToolType toolType);
+        [SerializeField] private AudioBundle alienSounds;
+        [SerializeField] private AudioBundle hammerSounds;
+        [SerializeField] private AudioBundle needleSounds;
+        [SerializeField] private AudioBundle taserSounds;
+
+        public virtual void OnToolUsed(ToolType toolType)
+        {
+            switch (toolType)
+            {
+                case ToolType.Alien:
+                    break;
+                case ToolType.Hammer:
+                    break;
+                case ToolType.Needle:
+                    break;
+                case ToolType.Taser:
+                    break;
+                default:
+                    Debug.LogWarning($"Tool type {toolType} is not supported");
+                    break;
+            }
+        }
     }
 }
