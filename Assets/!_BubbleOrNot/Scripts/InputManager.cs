1f63d9a50de7e96eba3657530bb3c03028c92338
KitKat <katjastoroygard@gmail.com>
2025-01-25 15:24:37 +0100

    Update InputManager.cs

--- Assets/!_BubbleOrNot/Scripts/InputManager.cs
+++ Assets/!_BubbleOrNot/Scripts/InputManager.cs
@@ -7,6 +7,9 @@ namespace BubbleOrNot.Runtime
     [AddComponentMenu("")]
     public class InputManager : MonoBehaviour
     {
+        [SerializeField] private ToolManager toolManager;
+        
+        
         private Camera _mainCamera;
 
         private void Awake()
@@ -16,12 +19,28 @@ namespace BubbleOrNot.Runtime
 
         public void OnClick(InputAction.CallbackContext context)
         {
+            if (context.started) toolManager.OnClick(true);
+            if (context.canceled) toolManager.OnClick(false);
+            
             if (!context.started) return;
             
             RaycastHit2D rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
             if (!rayHit.collider) return;
             
-            Debug.Log(rayHit.collider.gameObject.name);
+            Debug.Log("Clicked collider with name: " + rayHit.collider.gameObject.name);
+
+            if (!toolManager) return;
+            
+            var prop = rayHit.collider.GetComponent<Prop>();
+            if (prop) toolManager.OnClickedProp(prop);
+            
+            var tool = rayHit.collider.GetComponent<Tool>();
+            if (tool) toolManager.OnClickedTool(tool);
+        }
+
+        public void OnDrop(InputAction.CallbackContext context)
+        {
+            toolManager.OnDrop();
         }
     }
 }
