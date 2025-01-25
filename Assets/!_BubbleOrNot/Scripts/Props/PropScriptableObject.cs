c742627ddd83c1cbce1f37561af603780c895b67
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:25:29 +0100

    scriptable object stuff

---
+++ Assets/!_BubbleOrNot/Scripts/Props/PropScriptableObject.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+namespace BubbleOrNot.Runtime
+{
+    [CreateAssetMenu(menuName = "BubbleOrNot/Prop", fileName = "PropName")]
+    public class PropScriptableObject : ScriptableObject
+    {
+        public void OnToolUsed(ToolType toolType)
+        {
+            
+        }
+    }
+}
