22412c91713d572c486069a31105882c9ebcec9f
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:55:18 +0100

    Update AudioManager.cs

--- Assets/!_BubbleOrNot/Scripts/Audio/AudioManager.cs
+++ Assets/!_BubbleOrNot/Scripts/Audio/AudioManager.cs
@@ -6,6 +6,17 @@ namespace BubbleOrNot.Runtime.Audio
     [AddComponentMenu("")]
     public class AudioManager : MonoBehaviour
     {
+        public static AudioManager Instance { get; private set; }
 
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(this);
+                return;
+            }
+            
+            Instance = this;
+        }
     }
 }
