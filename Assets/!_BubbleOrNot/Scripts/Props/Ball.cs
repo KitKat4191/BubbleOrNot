5121c712bbe61957e734fd697f03cf9dbc1e3fc1
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:32:07 +0100

    created Ball.cs

---
+++ Assets/!_BubbleOrNot/Scripts/Props/Ball.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+namespace BubbleOrNot.Runtime
+{
+    [AddComponentMenu("BubbleOrNot/Props/Ball")]
+    public class Ball : Prop
+    {
+        public override void OnToolUsed(ToolType toolType)
+        {
+            
+        }
+    }
+}
