d4a1eac2109a1cb17f52531de6d64094536686a6
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:43:19 +0100

    created ExtensionUtils.cs

---
+++ Assets/!_BubbleOrNot/Scripts/ExtensionUtils.cs
@@ -0,0 +1,20 @@
+
+using System.Collections.Generic;
+
+namespace BubbleOrNot.Utils
+{
+    public static class ExtensionUtils
+    {
+        private static readonly System.Random Random = new();
+
+        public static void Shuffle<T>(this IList<T> list)  
+        {  
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = Random.Next(n + 1);  
+                (list[k], list[n]) = (list[n], list[k]);
+            }  
+        }
+    }
+}
