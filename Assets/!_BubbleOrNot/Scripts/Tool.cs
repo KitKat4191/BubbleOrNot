9fe469ec635d560f30b642919006b50fd445dcf1
KitKat <katjastoroygard@gmail.com>
2025-01-25 17:11:21 +0100

    Update Tool.cs

--- Assets/!_BubbleOrNot/Scripts/Tool.cs
+++ Assets/!_BubbleOrNot/Scripts/Tool.cs
@@ -6,7 +6,11 @@ namespace BubbleOrNot.Runtime
     [AddComponentMenu("BubbleOrNot/Tool")]
     public class Tool : MonoBehaviour
     {
+        private static readonly int Using = Animator.StringToHash("Using");
+        
+        
         [SerializeField] private ToolType toolType;
+        [SerializeField] private Animator animator;
         
         
         private Vector3 _spawnPosition;
@@ -22,9 +26,19 @@ namespace BubbleOrNot.Runtime
             transform.position = _spawnPosition;
         }
 
-        public void OnClick(bool pressed)
+        public void OnEquipped()
         {
             
+        }
+
+        public void OnUnequipped()
+        {
+            
+        }
+
+        public void OnClick(bool pressed)
+        {
+            animator.SetBool(Using, pressed);
         }
     }
 }
