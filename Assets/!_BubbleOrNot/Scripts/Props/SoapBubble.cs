fd6eef50f21fbc0ca4497fb328d96acd8f7d7f55
KitKat <katjastoroygard@gmail.com>
2025-01-25 17:58:27 +0100

    Update SoapBubble.cs

--- Assets/!_BubbleOrNot/Scripts/Props/SoapBubble.cs
+++ Assets/!_BubbleOrNot/Scripts/Props/SoapBubble.cs
@@ -6,9 +6,18 @@ namespace BubbleOrNot.Runtime
     [AddComponentMenu("BubbleOrNot/Props/SoapBubble")]
     public class SoapBubble : Prop
     {
+        [SerializeField] private float timeBeforeDestroy;
+        
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            base.OnCollisionEnter2D(other);
+            Destroy(gameObject, timeBeforeDestroy);
+        }
+
         public override void OnToolUsed(ToolType toolType)
         {
-            
+            base.OnToolUsed(toolType);
+            Destroy(gameObject, timeBeforeDestroy);
         }
     }
 }
