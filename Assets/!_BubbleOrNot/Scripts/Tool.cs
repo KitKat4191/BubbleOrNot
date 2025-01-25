040f3bbe5db91658a116ec009818a4e70a820733
KitKat <katjastoroygard@gmail.com>
2025-01-25 13:29:03 +0100

    created Tool.cs

---
+++ Assets/!_BubbleOrNot/Scripts/Tool.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+namespace BubbleOrNot.Runtime
+{
+    [AddComponentMenu("BubbleOrNot/Tool")]
+    public class Tool : MonoBehaviour
+    {
+        [SerializeField] private ToolType toolType;
+
+        public void OnUse()
+        {
+            
+        }
+    }
+}
