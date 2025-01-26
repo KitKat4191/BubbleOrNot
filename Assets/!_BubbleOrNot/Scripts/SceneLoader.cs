1dea1d5ceb63fee457af135713e5fdba553c559e
KitKat <katjastoroygard@gmail.com>
2025-01-26 11:41:31 +0100

    Update SceneLoader.cs

--- Assets/!_BubbleOrNot/Scripts/SceneLoader.cs
+++ Assets/!_BubbleOrNot/Scripts/SceneLoader.cs
@@ -2,11 +2,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using JetBrains.Annotations;
+
 namespace BubbleOrNot.Runtime
 {
     [AddComponentMenu("")]
     public class SceneLoader : MonoBehaviour
     {
+        [PublicAPI]
         public void LoadScene(string nameOfSceneToLoad)
         {
             SceneManager.LoadScene(nameOfSceneToLoad);
